ef8c430b0aa7f66e994da2e555cc768e95969f2c
Aleksey6453 <login6453@gmail.com>
2023-12-24 18:34:41 +0200

    add last changes this changes dont work

--- app/films/[filmid]/page.jsx
+++ app/films/[filmid]/page.jsx
@@ -1,8 +1,24 @@
+import { getAllFilms } from "@/app/actions/getAllFilms";
 import { getFilm } from "@/app/actions/getFilm";
 
 export default async function Film({filmId}){
     const film = await getFilm(filmId)
 
-    console.log(film)
+    return(
+        <div>
+            <h1 className="title">
+                {film.result.properties.title}
+            </h1>
+        </div>
+    )
+}
 
+export async function generateStaticParams(){
+    const films = await getAllFilms()
+
+    return(
+        films.result.map((film) => ({
+            filmId: film.uid
+        }) )
+    )
 }
